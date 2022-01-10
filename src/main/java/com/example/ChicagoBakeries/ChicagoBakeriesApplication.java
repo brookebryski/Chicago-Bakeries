93111340c1909cb6fc616ba48e72c3b11fd3a056
BrookeBryski <brookeabryski@gmail.com>
2022-01-10 14:17:05 -0600

    initial commit

---
+++ src/main/java/com/example/ChicagoBakeries/ChicagoBakeriesApplication.java
@@ -0,0 +1,24 @@
+package com.example.ChicagoBakeries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicagoBakeriesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicagoBakeriesApplication.class, args);
+	}
+
+}
+
+// 1: Tous Les Jours
+// 2: Sweet Mandy B's
+// 3: Maison Parisienne
+// 4: Alliance Bakery & Cafe
+// 5: Aya Pastry
+// 6: Bake
+// 7: Chiu Quon Bakery & Dim Sum
+// 8: Firecakes Donuts
+// 9: Bjorn Cakes
+// 10: Markellos Baking Company
---
+++ src/test/java/com/example/ChicagoBakeries/ChicagoBakeriesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ChicagoBakeries;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChicagoBakeriesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
