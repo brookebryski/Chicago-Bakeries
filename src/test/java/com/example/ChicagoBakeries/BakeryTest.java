15afc50bb661d16aa6883cb240db2c191e30ed7b
BrookeBryski <brookeabryski@gmail.com>
2022-01-10 15:15:57 -0600

    bakery test names and given when thens

--- src/test/java/com/example/ChicagoBakeries/BakeryTest.java
+++ src/test/java/com/example/ChicagoBakeries/BakeryTest.java
@@ -5,6 +5,29 @@ import org.junit.jupiter.api.Test;
 public class BakeryTest {
     @Test
     public void given1GetTousLesJours() {
+    // Given: I am a user
+    // When: I enter the string "1"
+    // Then: I get back the string "Tous Les Jours"
+    }
 
+    @Test
+    public void given5GetAyaPastry() {
+        // Given: I am a user
+        // When: I enter the string "5"
+        // Then: I get back the string "Aya Pastry"
+    }
+
+    @Test
+    public void given9GetBjornCakes() {
+        // Given: I am a user
+        // When: I enter the string "9"
+        // Then: I get back the string "Bjorn Cakes"
+    }
+
+    @Test
+    public void given10GetMarkellosBakingCompany() {
+        // Given: I am a user
+        // When: I enter the string "10"
+        // Then: I get back the string "Markellos Baking Company"
     }
 }
